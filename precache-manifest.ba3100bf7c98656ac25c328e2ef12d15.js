88dfbe4404e791ea7f51787fa183dd964c385621
unknown <footballandgermany@gmail.com>
2020-08-26 21:07:44 +0530

    Updates

---
+++ precache-manifest.ba3100bf7c98656ac25c328e2ef12d15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d404f88d074a1a29a7e95ddaa5dfc603",
+    "url": "/myNewsAI/index.html"
+  },
+  {
+    "revision": "092cdd1dd34a0e04230a",
+    "url": "/myNewsAI/static/css/main.77ba355e.chunk.css"
+  },
+  {
+    "revision": "11885413c549c162b8c3",
+    "url": "/myNewsAI/static/js/2.2a8d0501.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/myNewsAI/static/js/2.2a8d0501.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092cdd1dd34a0e04230a",
+    "url": "/myNewsAI/static/js/main.e7cc47a2.chunk.js"
+  },
+  {
+    "revision": "ba4d2ace7c70dff44296",
+    "url": "/myNewsAI/static/js/runtime-main.6340747e.js"
+  }
+]);
